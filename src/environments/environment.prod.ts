eae354508c639f96dd904dae4b2d345edb6a17b4
Odain Chevannes <Odain_Chevannes@sagicor.com>
2020-08-12 08:03:33 -0500

    remove environment.prod.json

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiHost: "167.86.101.106",
-  apiPort: "3000",
-  appTimeoutMilliSeconds: 15000,
-  KIOSK_LOCKER_ID : '5f2c431557fe3030d8637c05'
-};
